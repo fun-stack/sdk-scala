d5306f53b2e303e1cf1cd000a7ca64e3e0203c58
johannes karoff <johannes@karoff.net>
2021-07-26 17:55:27 +0200

    add authorization header to http requests

--- web/src/main/scala/Http.scala
+++ web/src/main/scala/Http.scala
@@ -8,8 +8,20 @@ import sttp.model.Uri
 import scala.concurrent.ExecutionContext
 
 class Http(http: HttpAppConfig) {
+  private var currentToken = Option.empty[String]
+  Fun.auth.foreach { auth =>
+    auth.currentUser.foreach { user =>
+      currentToken = user.map(_.token.access_token)
+    }
+  }
+
   private implicit val cs = IO.contextShift(ExecutionContext.global)
-  private val backend     = FetchCatsBackend[IO]()
+  private val backend = FetchCatsBackend[IO](customizeRequest = request => {
+    currentToken.foreach { token =>
+      request.headers.append("Authorization", s"Bearer $token")
+    }
+    request
+  })
 
   def client[I, E, O](endpoint: Endpoint[I, E, O, Any]): I => IO[Either[E, O]] =
     SttpClientInterpreter.toClientThrowDecodeFailures[IO, I, E, O, Any](endpoint, Some(Uri.unsafeApply("https", http.domain)), backend)
