d91f68440dd01de0e547ee84c0c9fb595941582a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-09-17 23:54:41 +0200

    Update scalatest to 3.2.10 (#20)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ object Deps {
   import Def.{setting => s}
 
   // testing
-  val scalatest = s("org.scalatest" %%% "scalatest" % "3.2.9")
+  val scalatest = s("org.scalatest" %%% "scalatest" % "3.2.10")
 
   // core libraries
   val cats = new {
