306e87733d5c9d5b2587d33d4c4eb076e522b869
Scala Steward <me@scala-steward.org>
2021-09-17 16:22:06 +0200

    Update scalatest to 3.2.9

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ object Deps {
   import Def.{setting => s}
 
   // testing
-  val scalatest = s("org.scalatest" %%% "scalatest" % "3.2.0")
+  val scalatest = s("org.scalatest" %%% "scalatest" % "3.2.9")
 
   // core libraries
   val cats = new {
