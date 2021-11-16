2f970bdd1d14b3eab7564d4b859fbef1b2cbb18f
Felix Dietze <felix.dietze@rwth-aachen.de>
2021-11-16 19:20:01 +0000

    Use ws for localhost instead wss

--- web/src/main/scala/Api.scala
+++ web/src/main/scala/Api.scala
@@ -7,7 +7,9 @@ import chameleon.{Serializer, Deserializer}
 import funstack.core._
 
 class Api(api: ApiAppConfig) {
-  val ws = new Websocket(WebsocketConfig(baseUrl = Url(s"wss://${api.domain}"), allowUnauthenticated = api.allowUnauthenticated))
+  private val isLocalhost = api.domain.startsWith("localhost:") || api.domain == "localhost"
+  private val protocol = if(isLocalhost) "ws" else "wss"
+  val ws = new Websocket(WebsocketConfig(baseUrl = Url(s"${protocol}://${api.domain}"), allowUnauthenticated = api.allowUnauthenticated))
 
   def wsClient[PickleType, F[_]: Async](implicit
       serializer: Serializer[ClientMessage[PickleType], String],
