74d88bb5865d06ed091557a2c008f8aeb65e20fb
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-01-30 04:16:03 +0100

    Update colibri to 0.2.3 (#81)

--- project/Deps.scala
+++ project/Deps.scala
@@ -14,7 +14,7 @@ object Deps {
   }
 
   // frp
-  val colibri = s("com.github.cornerman" %%% "colibri" % "0.2.2")
+  val colibri = s("com.github.cornerman" %%% "colibri" % "0.2.3")
 
   // rpc
   val sloth     = s("com.github.cornerman" %%% "sloth" % "0.4.0")
