52e2b927f420062752b26190a9edf1c545c6813f
johannes karoff <johannes@karoff.net>
2022-02-25 13:30:44 +0100

    use native js base64 implementation

--- core/src/main/scala/StringSerdes.scala
+++ core/src/main/scala/StringSerdes.scala
@@ -2,27 +2,18 @@ package funstack.core
 
 import chameleon.{Serializer, Deserializer}
 import java.nio.ByteBuffer
-import com.github.marklister.base64.Base64._
 import scala.util.Try
 
 case class StringSerdes(value: String)
 object StringSerdes {
   implicit def bytesSerializer[T: Serializer[*, ByteBuffer]]: Serializer[T, StringSerdes] =
     Serializer[T, ByteBuffer].mapSerialize { bytes =>
-      val byteArray =
-        if (bytes.hasArray) bytes.array
-        else {
-          val array = new Array[Byte](bytes.remaining)
-          bytes.rewind()
-          bytes.get(array)
-          array
-        }
-      StringSerdes(byteArray.toBase64)
+      StringSerdes(Base64Codec.encode(bytes))
     }
 
   implicit def bytesDeserializer[T: Deserializer[*, ByteBuffer]]: Deserializer[T, StringSerdes] =
     Deserializer[T, ByteBuffer].flatmapDeserialize { base64 =>
-      Try(base64.value.toByteArray).toEither.map(ByteBuffer.wrap)
+      Try(Base64Codec.decode(base64.value)).toEither
     }
 
   implicit def stringSerializer[T: Serializer[*, String]]: Serializer[T, StringSerdes] =
