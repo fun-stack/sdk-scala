7ec1aeff0b0a1bd3f31815ad70d6903a3d16bdb9
johannes karoff <johannes@karoff.net>
2022-03-12 17:18:28 +0100

    local mocking

--- backend/src/main/scala/Fun.scala
+++ backend/src/main/scala/Fun.scala
@@ -3,14 +3,15 @@ package funstack.backend
 object Fun {
   val config = Config.load()
 
-  val authOption = config.fun.cognitoUserPoolId.map(new Auth(_))
+  val authOption =
+    config.fun.cognitoUserPoolId
+      .map(new AuthAws(_))
+      .orElse(config.fun.devEnvironment.flatMap(_.getEmail.toOption).map(new AuthDev(_)))
 
   val wsOption =
     config.fun.eventsSnsTopic
       .map(new WsOperationsAWS(_))
-      .orElse(config.fun.devEnvironment.flatMap { env =>
-        env.sendSubscription.map(new WsOperationsDev(_)).toOption
-      })
+      .orElse(config.fun.devEnvironment.flatMap(_.sendSubscription.toOption).map(new WsOperationsDev(_)))
       .map(new Ws(_))
 
   case class MissingModuleException(name: String) extends Exception(s"Missing module: $name")
