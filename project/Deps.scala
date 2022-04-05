524a18a973ba614c56da272648674fc029be1d99
Scala Steward <me@scala-steward.org>
2022-04-06 00:24:53 +0200

    Update cats-effect to 3.3.11

--- project/Deps.scala
+++ project/Deps.scala
@@ -9,7 +9,7 @@ object Deps {
 
   // core libraries
   val cats = new {
-    val effect = s("org.typelevel" %%% "cats-effect" % "3.3.10")
+    val effect = s("org.typelevel" %%% "cats-effect" % "3.3.11")
   }
 
   // frp
