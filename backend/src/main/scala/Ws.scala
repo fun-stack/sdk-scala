01fa315ca37063bc5a24fe7965c35da096d7b471
johannes karoff <johannes@karoff.net>
2022-08-24 23:40:54 +0200

    add Fun.ws.sendTransportFuture

--- backend/src/main/scala/Ws.scala
+++ backend/src/main/scala/Ws.scala
@@ -3,14 +3,17 @@ package funstack.backend
 import funstack.ws.core.ServerMessageSerdes
 import funstack.core.{CanSerialize, SubscriptionEvent}
 import facade.amazonaws.services.sns._
+
 import scala.scalajs.js
 import sloth._
-
 import cats.data.Kleisli
 import cats.effect.IO
 
+import scala.concurrent.Future
+
 class Ws(operations: WsOperations) {
-  def sendTransport[T: CanSerialize] = new WsTransport[T](operations)
+  def sendTransport[T: CanSerialize]       = new WsTransport[T](operations)
+  def sendTransportFuture[T: CanSerialize] = new WsTransportFuture[T](operations)
 }
 
 private trait WsOperations {
@@ -49,4 +52,12 @@ class WsTransport[T: CanSerialize](operations: WsOperations) extends RequestTran
     val subscriptionKey = s"${request.path.mkString("/")}/${CanSerialize[T].serialize(request.payload)}"
     operations.sendToSubscription(subscriptionKey, SubscriptionEvent(subscriptionKey, CanSerialize[T].serialize(body)))
   }
+}
+
+class WsTransportFuture[T: CanSerialize](operations: WsOperations) extends RequestTransport[T, Kleisli[Future, *, Unit]] {
+  import cats.effect.unsafe.implicits.global
+
+  private val inner = new WsTransport[T](operations)
+
+  def apply(request: Request[T]): Kleisli[Future, T, Unit] = inner(request).mapF(_.unsafeToFuture())
 }
