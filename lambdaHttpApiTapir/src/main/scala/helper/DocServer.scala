b5ace709d32ce2d81c70667b5ac3b7eabbec2588
johannes karoff <johannes@karoff.net>
2022-09-30 01:40:15 +0200

    initialize oauth and provide a oauth2_redirect.html in swagger ui

--- lambdaHttpApiTapir/src/main/scala/helper/DocServer.scala
+++ lambdaHttpApiTapir/src/main/scala/helper/DocServer.scala
@@ -2,33 +2,40 @@ package funstack.lambda.http.api.tapir.helper
 
 import io.circe.syntax._
 import net.exoego.facade.aws_lambda._
+import sttp.apispec.openapi.Info
 import sttp.apispec.openapi.circe._
 import sttp.tapir.docs.openapi.{OpenAPIDocsInterpreter, OpenAPIDocsOptions}
 import sttp.tapir.server.ServerEndpoint
 
 import scala.scalajs.js
 
-case class DocInfo(title: String, version: String)
+case class DocInfo(info: Info)
 object DocInfo {
   def default = DocInfo(title = "API", version = "latest")
+
+  def apply(title: String, version: String): DocInfo = DocInfo(Info(title = title, version = version))
 }
 
 object DocServer {
 
-  def result(body: String, contentType: String): APIGatewayProxyStructuredResultV2 = APIGatewayProxyStructuredResultV2(
+  private def result(body: String, contentType: String): APIGatewayProxyStructuredResultV2 = APIGatewayProxyStructuredResultV2(
     statusCode = 200,
     body = body,
     headers = js.Dictionary("Content-Type" -> contentType).asInstanceOf[HeadersBDS],
   )
 
-  def serve[F[_]](path: List[String], endpoints: List[ServerEndpoint[_, F]], info: DocInfo): Option[APIGatewayProxyStructuredResultV2] = path match {
+  def serve[F[_]](path: List[String], endpoints: List[ServerEndpoint[_, F]], docInfo: DocInfo): Option[APIGatewayProxyStructuredResultV2] = path match {
     case Nil | List("index.html") =>
-      val html = SwaggerUI.html(info.title, "openapi.json")
+      val html = SwaggerUI.html(docInfo.info.title, "openapi.json")
+      Some(result(html, "text/html"))
+
+    case List("oauth2_redirect.html") =>
+      val html = SwaggerUI.oauth2RedirectHtml(docInfo.info.title)
       Some(result(html, "text/html"))
 
     case List("openapi.json") =>
       val openapi = OpenAPIDocsInterpreter(OpenAPIDocsOptions.default)
-        .serverEndpointsToOpenAPI[F](endpoints, title = info.title, version = info.version)
+        .serverEndpointsToOpenAPI[F](endpoints, docInfo.info)
       val json    = openapi.asJson.spaces2SortKeys
       Some(result(json, "application/json"))
 
--- lambdaHttpApiTapir/src/main/scala/helper/SwaggerUI.scala
+++ lambdaHttpApiTapir/src/main/scala/helper/SwaggerUI.scala
@@ -1,7 +1,5 @@
 package funstack.lambda.http.api.tapir.helper
 
-import scala.scalajs.js
-
 object SwaggerUI {
   private val version = "4"
 
@@ -13,10 +11,17 @@ object SwaggerUI {
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta
     name="description"
-    content="SwaggerUI"
+    content="${title}"
   />
   <title>${title}</title>
   <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@${version}/swagger-ui.css" />
+  <style>input#client_secret, label[for="client_secret"] { display: none; }</style>
+  <script>
+    const appHost = window.location.host.replace(/^api\\./, "");
+    const elem = document.createElement('script');
+    elem.src = window.location.protocol + "//" + appHost + "/app_config.js";
+    document.head.appendChild(elem);
+  </script>
 </head>
 <body>
 <div id="swagger-ui"></div>
@@ -27,9 +32,98 @@ object SwaggerUI {
       url: '${openApiSpecPath}',
       dom_id: '#swagger-ui',
     });
+
+    if (AppConfig && AppConfig.auth) {
+      window.ui.initOAuth({
+        appName: '${title}',
+        clientId: AppConfig.auth.clientId,
+        scopes: "user/api"
+      });
+    }
   };
 </script>
 </body>
 </html>"""
 
+  def oauth2RedirectHtml(title: String) = s"""
+<!doctype html>
+<html lang="en-US">
+<head>
+    <title>${title}: OAuth2 Redirect</title>
+</head>
+<body>
+<script>
+    'use strict';
+    function run () {
+        var oauth2 = window.opener.swaggerUIRedirectOauth2;
+        var sentState = oauth2.state;
+        var redirectUrl = oauth2.redirectUrl;
+        var isValid, qp, arr;
+
+        if (/code|token|error/.test(window.location.hash)) {
+            qp = window.location.hash.substring(1).replace('?', '&');
+        } else {
+            qp = location.search.substring(1);
+        }
+
+        arr = qp.split("&");
+        arr.forEach(function (v,i,_arr) { _arr[i] = '"' + v.replace('=', '":"') + '"';});
+        qp = qp ? JSON.parse('{' + arr.join() + '}',
+                function (key, value) {
+                    return key === "" ? value : decodeURIComponent(value);
+                }
+        ) : {};
+
+        isValid = qp.state === sentState;
+
+        if ((
+          oauth2.auth.schema.get("flow") === "accessCode" ||
+          oauth2.auth.schema.get("flow") === "authorizationCode" ||
+          oauth2.auth.schema.get("flow") === "authorization_code"
+        ) && !oauth2.auth.code) {
+            if (!isValid) {
+                oauth2.errCb({
+                    authId: oauth2.auth.name,
+                    source: "auth",
+                    level: "warning",
+                    message: "Authorization may be unsafe, passed state was changed in server. The passed state wasn't returned from auth server."
+                });
+            }
+
+            if (qp.code) {
+                delete oauth2.state;
+                oauth2.auth.code = qp.code;
+                oauth2.callback({auth: oauth2.auth, redirectUrl: redirectUrl});
+            } else {
+                let oauthErrorMsg;
+                if (qp.error) {
+                    oauthErrorMsg = "["+qp.error+"]: " +
+                        (qp.error_description ? qp.error_description+ ". " : "no accessCode received from the server. ") +
+                        (qp.error_uri ? "More info: "+qp.error_uri : "");
+                }
+
+                oauth2.errCb({
+                    authId: oauth2.auth.name,
+                    source: "auth",
+                    level: "error",
+                    message: oauthErrorMsg || "[Authorization failed]: no accessCode received from the server."
+                });
+            }
+        } else {
+            oauth2.callback({auth: oauth2.auth, token: qp, isValid: isValid, redirectUrl: redirectUrl});
+        }
+        window.close();
+    }
+
+    if (document.readyState !== 'loading') {
+        run();
+    } else {
+        document.addEventListener('DOMContentLoaded', function () {
+            run();
+        });
+    }
+</script>
+</body>
+</html>
+"""
 }
