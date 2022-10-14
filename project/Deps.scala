d8edee1b48e24ab3f35a11f1398a6422289a8f92
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-10-15 00:09:09 +0200

    Update scalatest to 3.2.14 (#164)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ object Deps {
   import Def.{setting => s}
 
   // testing
-  val scalatest = s("org.scalatest" %%% "scalatest" % "3.2.13")
+  val scalatest = s("org.scalatest" %%% "scalatest" % "3.2.14")
 
   // core libraries
   val cats = new {
