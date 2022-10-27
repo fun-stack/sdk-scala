90ea6797c50a44d62dbf391966e29183125554ea
johannes karoff <johannes@karoff.net>
2022-10-27 12:05:16 +0200

    add urls to client auth interface

--- clientCore/src/main/scala/auth/Auth.scala
+++ clientCore/src/main/scala/auth/Auth.scala
@@ -3,5 +3,9 @@ package funstack.client.core.auth
 import colibri.Observable
 
 trait Auth {
+  def loginUrl: String
+  def signupUrl: String
+  def logoutUrl: String
+
   def currentUser: Observable[Option[User]]
 }
