d6bae0277dad229f7d9232f4eec610714d792ec3
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-06-14 17:55:12 +0200

    Update sloth to 0.7.3 (#280)

--- project/Deps.scala
+++ project/Deps.scala
@@ -20,7 +20,7 @@ object Deps {
   }
 
   // rpc
-  val sloth     = s("com.github.cornerman" %%% "sloth" % "0.7.1")
+  val sloth     = s("com.github.cornerman" %%% "sloth" % "0.7.3")
   val chameleon = s("com.github.cornerman" %%% "chameleon" % "0.4.1")
 
   // websocket connecitivity
